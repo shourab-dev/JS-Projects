d1ddbed5495b43f8b1f48bb7ec608781eac177c1
shourab <shourab.cit.bd@gmail.com>
2023-01-10 20:27:55 +0600

    Dark Mode js Fully Done

--- Dark Mode Js/js/app.js
+++ Dark Mode Js/js/app.js
@@ -6,10 +6,38 @@ const darkModeToggler = (e) => {
 
   if (!hasClass) {
     body.classList.add(darkModeClass);
-    console.log(hasClass);
+    localStorage.setItem("theme", darkModeClass);
+    return false;
+  }
+  localStorage.setItem("theme", "lightmode");
+  body.classList.remove(darkModeClass);
+};
+
+togglerBtn.addEventListener("click", darkModeToggler);
+
+//* THEME SCHEMA SAVES ON RELOAD
+const defaultThemeSwitcher = () => {
+  if (localStorage.getItem("theme") == darkModeClass) {
+    body.classList.add(darkModeClass);
     return false;
   }
   body.classList.remove(darkModeClass);
 };
 
-togglerBtn.addEventListener("click", darkModeToggler);
+//* THEME SCHEMA BASED ON SYSTEM
+const systemThemeSchema = () => {
+  let theme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+  if (localStorage.getItem("theme") == null) {
+    if (theme) {
+      body.classList.add(darkModeClass);
+      localStorage.setItem("theme", darkModeClass);
+      return false;
+    }
+    localStorage.removeItem("theme");
+    body.classList.remove(darkModeClass);
+  } else {
+    defaultThemeSwitcher();
+  }
+};
+
+window.addEventListener("DOMContentLoaded", systemThemeSchema);
